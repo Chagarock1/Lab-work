1121a4b41b00c9a0867092d5607e8cf636571b24
Chagarock1 <yaroslav.khrabatyn-ki2401@kep.nung.edu.ua>
2026-09-19 15:03:25 +0300

    Rename sum function to calc_sum and update calls

--- labs/lab3/1.py
+++ labs/lab3/1.py
@@ -1,4 +1,4 @@
-def sum(n, skip):
+def calc_sum(n, skip):
     total = 0
     for i in range(1, n + 1):
         if i % skip != 0:
@@ -6,7 +6,12 @@ def sum(n, skip):
     return total
 
 skip_number = 3
+results = []
 
-for n in [10, 15, 20]:
-    result = sum(n, skip_number)
-    print("При n =", n, "сума дорівнює:", result)
+for n in range(10, 41, 10):
+    res = calc_sum(n, skip_number)
+    results.append(res)
+    print("При n =", n, "сума дорівнює:", res)
+
+if results[-1] > results[0]:
+    print("Сума зростає зі збільшенням n")
