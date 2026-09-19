e3faa80f1d619f712e0a2795e8c7b63f91c36e17
Chagarock1 <yaroslav.khrabatyn-ki2401@kep.nung.edu.ua>
2026-09-19 15:04:21 +0300

    Rename function and update print statements

--- labs/lab3/2.py
+++ labs/lab3/2.py
@@ -1,7 +1,4 @@
-is_multiple_of_10 = lambda x: x % 10 == 0
+check_10 = lambda x: x % 10 == 0
 
-for number in range(1, 11):
-    print(f"Число {number} кратне 10: {is_multiple_of_10(number)}")
-
-for number in range(10, 101, 10):
-    print(f"Число {number} кратне 10: {is_multiple_of_10(number)}")
+for n in range(1, 11):
+    print("Число", n, "кратне 10:", check_10(n))
