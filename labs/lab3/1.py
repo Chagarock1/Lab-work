f439f2149a8b5be7936d7a72a31a557871286dfa
Chagarock1 <yaroslav.khrabatyn-ki2401@kep.nung.edu.ua>
2026-09-22 15:50:39 +0300

    Refactor sum calculation and input handling

--- labs/lab3/1.py
+++ labs/lab3/1.py
@@ -1,17 +1,14 @@
-def calc_sum(n, skip):
-    total = 0
+def suma(n, k):
+    s = 0
+
     for i in range(1, n + 1):
-        if i % skip != 0:
-            total = total + i
-    return total
+        if i % k != 0:
+            s += i
 
-skip_number = 3
-results = []
+    return s
 
-for n in range(10, 41, 10):
-    res = calc_sum(n, skip_number)
-    results.append(res)
-    print("При n =", n, "сума дорівнює:", res)
 
-if results[-1] > results[0]:
-    print("Сума зростає зі збільшенням n")
+n = int(input("Введіть n: "))
+k = int(input("Введіть число, кратні якому пропускати: "))
+
+print("Сума:", suma(n, k))
